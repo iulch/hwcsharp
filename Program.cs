66b8e8d4eec4610cbf3537f9a41640bce0836c77
iulch <126890303+iulch@users.noreply.github.com>
2023-03-10 19:30:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class programm
+{
+    public static void Main()
+    {
+        Console.WriteLine("Write here your name");
+        string name -Console.ReadLine();
+        Console.WriteLine(name + ", Hello!");
+    }
+}
